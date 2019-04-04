4655218a11bd8f25bc00ac720822793cfabd07eb
lyanhao <48517015+lyanhao@users.noreply.github.com>
2019-04-04 17:31:48 +0800

    Update Swap_Number_without_tem.c

--- Day2/Day2/Swap_Number_without_tem.c
+++ Day2/Day2/Swap_Number_without_tem.c
@@ -9,15 +9,15 @@ void Swap_without_temp(int* x, int* y)
 	*y = *x - *y;//j=5-2=3
 	*x = *x - *y;//i=5-3=2
 }
-int main2()
+int main()
 {
 	int num_1 = 0; 
 	int num_2 = 0;
-	printf("请输入两个数num_1，num_2\n");
-	scanf("%d %d", &num_1, &num_2);//例如i=3，j=2
-	printf("交换前Num1=%d，Num2=%d\n",num_1, num_2);
+	printf("璇疯緭鍏ヤ袱涓暟num_1锛宯um_2\n");
+	scanf("%d %d", &num_1, &num_2);//渚嬪i=3锛宩=2
+	printf("浜ゆ崲鍓峃um1=%d锛孨um2=%d\n",num_1, num_2);
 	Swap_without_temp(&num_1, &num_2);
-	printf("交换后Num1=%d，Num2=%d\n", num_1, num_2);
+	printf("浜ゆ崲鍚嶯um1=%d锛孨um2=%d\n", num_1, num_2);
 	system("pause");
 	return 0;
 }
