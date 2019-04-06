c086e65327a9253584d7bbfe05c573c0f10a684b
lyanhao <48517015+lyanhao@users.noreply.github.com>
2019-04-06 23:33:11 +0800

    Update Judge_Leap_Year.c

--- Day1/Day1/Judge_Leap_Year.c
+++ Day1/Day1/Judge_Leap_Year.c
@@ -2,18 +2,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
+
+//判断1000年---2000年之间的闰年 
+
+int main(){
 	int year;
 	int i = 0;
-	for (year = 1000; year <= 2000; year++)
-	{
-		if ((year % 400) == 0 || ((year % 4 == 0) && (year % 100 != 0)))
-		{
+	for (year = 1000; year <= 2000; year++){
+		if ((year % 400) == 0 || ((year % 4 == 0) && (year % 100 != 0))){
 			printf("%7d", year);
 			i++;
-			if (i % 10 == 0)
-			{
+			if (i % 10 == 0){
 				printf("\n");
 			}
 		}
