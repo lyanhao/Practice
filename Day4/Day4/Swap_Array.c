140f53d1bd6d1745e086b16ee7ace0bfff6ec8cf
李 <1094361426@qq.com>
2019-04-06 23:16:45 +0800

    1/1-1/2+1/3-1/4+1/5 …… + 1/99 - 1/100

--- Day4/Day4/Swap_Array.c
+++ Day4/Day4/Swap_Array.c
@@ -1,41 +1,41 @@
-﻿#define _CRT_SECURE_NO_WARNINGS 1
-
-#define N 5 //自定数组大小
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-	int arr1[N] = { 0 };
-	int arr2[N] = { 0 };
-	int tmp = 0;
-	int i = 0;
-
-	printf("请输入arr1数组元素：\n");
-	for (i = 0; i < N; i++){
-		scanf("%d", &arr1[i]);
-	}
-
-	printf("请输入arr2数组元素：\n");
-	for (i = 0; i < N; i++){
-		scanf("%d", &arr2[i]);
-	}
-
-	printf("交换后的数组元素为：\n");
-	for (i = 0; i < N; i++){
-		tmp = arr1[i];
-		arr1[i] = arr2[i];
-		arr2[i] = tmp;
-	}
-	for (i = 0; i < N; i++){
-		printf("%3d", arr1[i]);
-	}
-
-	printf("\n");
-	for (i = 0; i < N; i++){
-		printf("%3d", arr2[i]);
-	}
-
-	printf("\n");
-	system("pause");
-	return 0;
-}
+﻿//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#define N 5 //自定数组大小
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	int arr1[N] = { 0 };
+//	int arr2[N] = { 0 };
+//	int tmp = 0;
+//	int i = 0;
+//
+//	printf("请输入arr1数组元素：\n");
+//	for (i = 0; i < N; i++){
+//		scanf("%d", &arr1[i]);
+//	}
+//
+//	printf("请输入arr2数组元素：\n");
+//	for (i = 0; i < N; i++){
+//		scanf("%d", &arr2[i]);
+//	}
+//
+//	printf("交换后的数组元素为：\n");
+//	for (i = 0; i < N; i++){
+//		tmp = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = tmp;
+//	}
+//	for (i = 0; i < N; i++){
+//		printf("%3d", arr1[i]);
+//	}
+//
+//	printf("\n");
+//	for (i = 0; i < N; i++){
+//		printf("%3d", arr2[i]);
+//	}
+//
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
