faca85941d8e3a1bba47508f8f3a82c0d3fe969d
lyanhao <48517015+lyanhao@users.noreply.github.com>
2019-04-22 23:18:38 +0800

    Update Swap_Number.c

--- Day2/Day2/Swap_Number.c
+++ Day2/Day2/Swap_Number.c
@@ -3,24 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Swap(int* x, int* y)
-{
+void Swap(int* x, int* y) {
 	int temp;
 	temp = *x;
 	*x = *y;
 	*y = temp;
 }
-int main()
-{
+
+int main() {
 	int num_1 = 0;
 	int num_2 = 0;
+	
 	printf("请输入num1的值：");
 	scanf("%d",&num_1);
 	printf("请输入num2的值：");
 	scanf("%d", &num_2);
+	
 	printf("交换前Num1=%d,Num2=%d\n", num_1, num_2);
 	Swap(&num_1, &num_2);
 	printf("交换后Num1=%d,Num2=%d\n", num_1, num_2);
+	
 	system("pause");
 	return 0;
 }
