bae68e3cb47d71b305d5e1a5faa89eda1e89b346
lyanhao <48517015+lyanhao@users.noreply.github.com>
2019-04-23 23:19:51 +0800

    Update Find_Greatest_Common_Divisor.c

--- Day3/Day3/Find_Greatest_Common_Divisor.c
+++ Day3/Day3/Find_Greatest_Common_Divisor.c
@@ -2,18 +2,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//ตÝน้
-int Greatest_Common_Divisor(int a, int b){
+
+int Greatest_Common_Divisor(int a, int b) {
 	if (b == 0) 
 		return a;
 	else return Greatest_Common_Divisor(b, a%b);
 }
 
-int main()
-{
+int main() {
 	int a, b;
-	while (scanf("%d%d", &a, &b) != 0){
-		printf("%d\n", Greatest_Common_Divisor(a, b));
+	while (scanf("%d%d", &a, &b) != 0) {
+	printf("%d\n", Greatest_Common_Divisor(a, b));
 	}
 	system("pause");
 	return 0;
