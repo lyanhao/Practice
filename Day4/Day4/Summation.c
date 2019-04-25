94274eb2bc0c7fc35f047d2205f92cc6f3de65f9
lyanhao <48517015+lyanhao@users.noreply.github.com>
2019-04-25 23:22:10 +0800

    Update Summation.c

--- Day4/Day4/Summation.c
+++ Day4/Day4/Summation.c
@@ -1,30 +1,30 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-////¼ÆËã1/1-1/2+1/3-1/4+1/5 ¡­¡­ + 1/99 - 1/100 µÄÖµ
-//
-//int main(){
-//	int i = 0;
-//	int j = 0;
-//	float Odd_Num = 0.0;   //¶¨ÒåÆæÊı·ÖÄ¸
-//	float Even_Num = 0.0;  //¶¨ÒåÅ¼Êı·ÖÄ¸
-//	float Sum = 0.0;       //¶¨ÒåºÍÖµ
-//	
-//	//¼ÆËãÆæÊı·ÖÄ¸µÄËùÓĞÊı×ÜºÍµÄÖµ
-//	for (i = 1; i <= 100; i += 2){
-//		Odd_Num += 1.0 / i;
-//	}
-//
-//	//¼ÆËãÅ¼Êı·ÖÄ¸µÄËùÓĞÊı×ÜºÍµÄÖµ
-//	for (j = 2; j <= 100; j += 2){
-//		Even_Num += 1.0 / j;
-//	}
-//
-//	//Çó×ÜºÍ
-//	Sum = Odd_Num - Even_Num;
-//
-//	printf("%f", Sum);
-//	system("pause");
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+//è®¡ç®—1/1-1/2+1/3-1/4+1/5 â€¦â€¦ + 1/99 - 1/100 çš„å€¼
+
+int main() {
+	int i = 0;
+	int j = 0;
+	float Odd_Num = 0.0;   //å®šä¹‰å¥‡æ•°åˆ†æ¯
+	float Even_Num = 0.0;  //å®šä¹‰å¶æ•°åˆ†æ¯
+	float Sum = 0.0;       //å®šä¹‰å’Œå€¼
+	
+	//è®¡ç®—å¥‡æ•°åˆ†æ¯çš„æ‰€æœ‰æ•°æ€»å’Œçš„å€¼
+	for (i = 1; i <= 100; i += 2) {
+		Odd_Num += 1.0 / i;
+	}
+
+	//è®¡ç®—å¶æ•°åˆ†æ¯çš„æ‰€æœ‰æ•°æ€»å’Œçš„å€¼
+	for (j = 2; j <= 100; j += 2) {
+		Even_Num += 1.0 / j;
+	}
+
+	//æ±‚æ€»å’Œ
+	Sum = Odd_Num - Even_Num;
+
+	printf("%f", Sum);
+	system("pause");
+	return 0;
+}
