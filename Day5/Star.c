323ea324478bcb2785941c2bd2689c7313e388e9
lyanhao <48517015+lyanhao@users.noreply.github.com>
2019-04-26 23:54:40 +0800

    Update Star.c

--- Day5/Star.c
+++ Day5/Star.c
@@ -3,30 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main() {
 	int i = 0;
 	int j = 0;
 	int n = 0;
 
 	scanf("%d", &n);
 
-	for (i = 1; i <= n; i++){
-		for (j = 0; j < n - i; j++){
+	for (i = 1; i <= n; i++) {
+		for (j = 0; j < n - i; j++) {
 			putchar(' ');
 		}
 
-		for (j = 0; j < 2 * i - 1; j++){
+		for (j = 0; j < 2 * i - 1; j++) {
 			putchar('*');
 		}
 		putchar('\n');
 	}
 
-	for (i = n - 1; i > 0; i--){
-		for (j = 0; j < n - i; j++){
+	for (i = n - 1; i > 0; i--) {
+		for (j = 0; j < n - i; j++) {
 			putchar(' ');
 		}
 
-		for (j = 0; j < 2 * i - 1; j++){
+		for (j = 0; j < 2 * i - 1; j++) {
 			putchar('*');
 		}
 		putchar('\n');
