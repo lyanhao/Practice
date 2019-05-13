639eee20c41cf24408307446bcd2757dc5914aad
lyanhao <48517015+lyanhao@users.noreply.github.com>
2019-05-13 23:09:42 +0800

    Update Print_Every_Num.c

--- Day9/Print_Every_Num.c
+++ Day9/Print_Every_Num.c
@@ -6,15 +6,13 @@
 
 void print(int n) {
 	if (n > 9) {
-		print(n / 10);
+	print(n / 10);
 	}
 	printf("%d  ", n % 10);
 }
 
 int main() {
-
 	int num = 0;
-	printf("ÇëÊäÈëÊı×Ö£º");
 	scanf("%d", &num);
 	print(num);
 	system("pause");
