176caf432d6e7eadcfb2ae5848911a36ae3755de
lyanhao <48517015+lyanhao@users.noreply.github.com>
2019-05-16 19:19:25 +0800

    Update Fibonacci_Number.c

--- Day9/Fibonacci_Number.c
+++ Day9/Fibonacci_Number.c
@@ -3,24 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD
-//1¡¢1¡¢2¡¢3¡¢5¡¢8¡¢13¡¢21¡¢34¡¢¡­¡­
-// F(n)=F(n-1)+F(n-2)£¨n>=3£¬n¡ÊN*£©
-
 //not recursive
-=======
-//1ã€1ã€2ã€3ã€5ã€8ã€13ã€21ã€34ã€â€¦â€¦
-// F(n)=F(n-1)+F(n-2)ï¼ˆn>=3ï¼ŒnâˆˆN*ï¼‰
+
 ///not recursive
->>>>>>> fb433d4bfb805eca6d723f9e578c96325810c1f1
-int fib1(int n){
+
+int fib1(int n) {
 	int an = 0, an_1 = 1, an_2 = 1, i = 0;
 
-	if (n <= 2){
+	if (n <= 2) {
 		return 1;
 	}
 
-	for (i = 2; i < n; i++){
+	for (i = 2; i < n; i++) {
 		an = an_1 + an_2;
 		an_2 = an_1;
 		an_1 = an;
@@ -29,29 +23,22 @@ int fib1(int n){
 }
 
 //recursive
-int fib2(int n){
-	if (n <= 2){
+int fib2(int n) {
+	if (n <= 2) {
 		return 1;
 	}
 	return fib2(n - 1) + fib2(n - 2);
 }
 
-int main(){
+int main() {
 	int n = 0;
-<<<<<<< HEAD
-	printf("ÇëÊäÈëÄãÒª²éÑ¯µÚ¼¸¸öì³²¨ÄÇÆõÊý£º");
-	scanf("%d",&n);
-	printf("%d\n", fib2(n));
-
-
-	system("pause");
-	return 0;
-}
-=======
-	printf("è¯·è¾“å…¥ä½ è¦æŸ¥è¯¢ç¬¬å‡ ä¸ªæ–æ³¢é‚£å¥‘æ•°ï¼š");
 	scanf("%d",&n);
 	printf("%d\n", fib2(n));
 	system("pause");
 	return 0;
 }
->>>>>>> fb433d4bfb805eca6d723f9e578c96325810c1f1
+	scanf("%d",&n);
+	printf("%d\n", fib2(n));
+	system("pause");
+	return 0;
+}
